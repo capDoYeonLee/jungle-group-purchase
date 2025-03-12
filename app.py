daf11cace7d107ad065bfbc7307e3e26339a20a6
Junhyuk Im <69585612+zeka0228@users.noreply.github.com>
2025-03-13 01:16:35 +0900

    게시글 수정 라우트 추가

--- app.py
+++ app.py
@@ -81,6 +81,35 @@ def buy_porduct(id):
         return jsonify({"result": "success", "message": "구매에 참여했습니다!!!"})
     else:
         return jsonify({"result": "fail", "message": "오류 발생으로 재시도 바랍니다."})
+#01:16 추가
+@app.route('/update_post', methods=['POST']) 
+def Update_post():
+    post_id = ObjectId(request.form['post_id']) 
+    update_boards = request.form['update_boards']
+    update_name = request.form['update_name']
+    update_link = request.form['update_link']
+    update_price = request.form['update_price']
+    update_deadline = request.form['update_deadline']
+    update_shipping = request.form['update_shipping']
+    update_condition = request.form['update_condition']
+    update_category = request.form['update_category']
+    result = db.boards.update_one(
+        {"_id": post_id},
+        {"$set": {
+            "board": update_boards,
+            "name": update_name,
+            "link": update_link,
+            "price": update_price ,
+            "deadline": update_deadline,
+            "shipping": update_shipping,
+            "condition": update_condition,
+            "category": update_category }}
+    )
+    if result.modified_count > 0:
+        return jsonify({"result": "success", "message": "수정되었습니다다!!!"})
+    else:
+        return jsonify({"result": "fail", "message": "오류 발생으로 재시도 바랍니다."})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
